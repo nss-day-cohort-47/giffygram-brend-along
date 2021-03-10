d2d9be5feb0bf4dc71fa6ab3b17d40337dcca5af
Brenda Long <brenda@studio6thave.com>
2021-03-10 08:58:29 -0600

    added eventlisteners

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,54 @@
-import { getUsers, getPosts} from "./data/DataManager.js"
-import { PostList } from "./feed/PostList.js"
+import { getUsers, getPosts } from "./data/DataManager.js";
+import { PostList } from "./feed/PostList.js";
+import { NavBar } from "./nav/NavBar.js";
+import { Footer } from "./nav/Footer.js";
 /**
  * Main logic module for what should happen on initial page load for Giffygram
  */
 
-//Get a reference to the location on the DOM where the app will display
-// let postElement = document.querySelector(".postList");
-// let navElement = document.querySelector("nav");
-// let entryElement = document.querySelector(".entryForm")
+const applicationElement = document.querySelector(".giffygram");
+const footerElement = document.querySelector("footer");
+
+footerElement.addEventListener("mousemove", event => {
+	console.log("moving now", event.x)
+})
+
+applicationElement.addEventListener("click", (event) => {
+	console.log('click edit');
+	
+	if (event.target.id.startsWith("edit")) {
+		console.log("post clicked", event.target.id.split("--"));
+		console.log("zero index value", event.target.id.split("--")[0]);
+		console.log("one index value, the id is", event.target.id.split("--")[1]);
+	}
+})
+
+applicationElement.addEventListener("change", event => {
+	// console.log('event obj', event);
+	if (event.target.id === "yearSelection") {
+	  const yearAsNumber =  parseInt(event.target.value)
+  
+	  console.log(`User wants to see posts since ${yearAsNumber}`)
+	}
+  })
 
 const showPostList = () => {
 	const postElement = document.querySelector(".postList");
-	  getPosts().then((allPosts) => {
-		  postElement.innerHTML = PostList(allPosts);
-	  })
-  }
+	getPosts().then((allPosts) => {
+		postElement.innerHTML = PostList(allPosts);
+	})
+}
 
+const showNavBar = () => {
+	//Get a reference to the location on the DOM where the nav will display
+	const navElement = document.querySelector("nav");
+	navElement.innerHTML = NavBar();
+}
+const showFooter = () => {
+	//Get a reference to the location on the DOM where the footer will display
+	const footerElement = document.querySelector("footer");
+	footerElement.innerHTML = Footer();
+}
 
 /*
 	This function performs one, specific task.
@@ -24,7 +57,9 @@ const showPostList = () => {
 	2. Are you defining the function here or invoking it?
 */
 const startGiffyGram = () => {
+	showNavBar();
 	showPostList();
+	showFooter();
 }
 // Are you defining the function here or invoking it?
 startGiffyGram();
